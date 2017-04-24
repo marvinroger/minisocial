c7bfda0af4c7afb3b7df08be39fdf70cdcbad870
Marvin Roger <nivramdu94@gmail.com>
2017-04-24 12:47:09 +0200

    :sparkles: Allow soft deletions of messages in the admin

--- app/admin.py
+++ app/admin.py
@@ -1,5 +1,21 @@
 from django.contrib import admin
 
-from .models import Message
+from .models import Message, MessageHistory
 
-admin.site.register(Message)
+class MessageAdmin(admin.ModelAdmin):
+    def save_model(self, request, obj, form, change):
+        super(MessageAdmin, self).save_model(request, obj, form, change)
+
+        if not change or 'is_deleted' not in form.changed_data:
+            return
+
+        is_deleted = form.cleaned_data['is_deleted']
+
+        # if just deleted: we add a delete history
+        # is just recovered: we remove the delete history
+        if is_deleted:
+            MessageHistory(message=obj, is_addition=False).save()
+        else:
+            MessageHistory.objects.get(message=obj, is_addition=False).delete()
+
+admin.site.register(Message, MessageAdmin)
